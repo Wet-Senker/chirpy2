29c99ed9549b5011d89dc4ff7a9af6a0a51fca6e
Wet-Senker <dhrnickdevries@hotmail.com>
2026-04-05 07:23:48 +0200

    ch67 afgerond

---
+++ internal/auth/make_jwt.go
@@ -0,0 +1,26 @@
+package auth
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:    "chirpy-access",
+		Subject:   userID.String(),
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn)),
+	})
+
+	signedJWT, err := token.SignedString([]byte(tokenSecret))
+	if err != nil {
+		return "", fmt.Errorf("error signing token: %w", err)
+	}
+	return signedJWT, nil
+
+}
